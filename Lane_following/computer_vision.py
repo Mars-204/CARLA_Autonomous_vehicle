f3b76b7b36757f06c92766fb8d0e705f86b1d688
Manthan Patel <105321297+Mars-204@users.noreply.github.com>
2023-11-12 14:03:47 +0100

    Update computer_vision.py

--- Lane_following/computer_vision.py
+++ Lane_following/computer_vision.py
@@ -11,6 +11,30 @@ import carla
 We expect from you to implement a run_step() function that will return data required by the Driver to steer the car.
 Our example uses RGB camera and OpenCV, but you can come up with any approach you wish
 """
+def show_speed(self, img):
+    global topspeed
+    h, w = img.shape[:2]
+    v = self.world.player.get_velocity()
+    speed = round((3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2)),2)
+    topspeed = speed if topspeed < speed else topspeed 
+    square_img = np.zeros_like(img, np.uint8)
+    square_img = cv2.rectangle(square_img, (40, 30), (300, 80), (255,255,255), cv2.FILLED)
+    img = cv2.addWeighted(img, 1, square_img, 0.2, 1)
+    
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 0.5 
+    color = (0, 0, 0)
+    thickness = 1
+    line1=f'Speed: {speed} km/h'
+    org1 = (50, 50)
+    line2=f'Top speed: {topspeed} km/h'
+    org2 = (50, 70)
+    img = cv2.putText(img, line1, org1, font, 
+               fontScale, color, thickness, cv2.LINE_AA)
+    img = cv2.putText(img, line2, org2, font, 
+               fontScale, color, thickness, cv2.LINE_AA)
+    
+    return img
 
 
 class Navigator(object):
@@ -38,7 +62,7 @@ class Navigator(object):
             opencv
         - However the surface array has swapped axes, so it cannot be used directly in opencv
         """
-        cv2.imshow("OpenCV camera view", self.camera_rgb.image)
+        cv2.imshow("OpenCV camera view", self.show_speed(self.camera_rgb.image))
         cv2.imshow("lidar", self.lidar.image)
         cv2.waitKey(1)
         
